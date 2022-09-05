26ffa2e9e2976f439f601223599d18ff0d1663b3
MohammadALShboul99 <110593677+MohammadALShboul99@users.noreply.github.com>
2022-09-05 22:30:23 +0300

    With CheckBoxListTile

--- lib/screens/Firstscreen.dart
+++ lib/screens/Firstscreen.dart
@@ -11,49 +11,67 @@ class My_first_Screen extends StatefulWidget {
 class _My_first_ScreenState extends State<My_first_Screen> {
   List MyItems = ["Dell", "Toshipa", "Sony", "Asus", "Hp", "Acer"];
   String selecteditem = "Dell";
+  bool myvalue = false;
+  bool myval = false;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Hello from 1st",
-                style: TextStyle(fontSize: 25),
-              ),
-              ElevatedButton.icon(
-                onPressed: () {
-                  setState(() {
-                    Navigator.pop(context, MaterialPageRoute(
-                      builder: (context) {
-                        return Mohammad();
-                      },
-                    ));
-                  });
-                },
-                icon: Icon(Icons.home),
-                label: Text("Go To Home"),
-              ),
-              DropdownButton(
-                  hint: Text("Please Select from This"),
-                  value: selecteditem,
-                  items: MyItems.map((e) => DropdownMenuItem(
-                        child: Text("$e"),
-                        value: e,
-                      )).toList(),
-                  onChanged: ((val) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Hello from 1st",
+                  style: TextStyle(fontSize: 25),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {
                     setState(() {
-                      selecteditem = val.toString();
+                      Navigator.pop(context, MaterialPageRoute(
+                        builder: (context) {
+                          return Mohammad();
+                        },
+                      ));
                     });
-                  }))
-            ],
+                  },
+                  icon: Icon(Icons.home),
+                  label: Text("Go To Home"),
+                ),
+                DropdownButton(
+                    hint: Text("Please Select from This"),
+                    value: selecteditem,
+                    items: MyItems.map((e) => DropdownMenuItem(
+                          child: Text("$e"),
+                          value: e,
+                        )).toList(),
+                    onChanged: ((val) {
+                      setState(() {
+                        selecteditem = val.toString();
+                      });
+                    })),
+                CheckboxListTile(
+                  value: myvalue,
+                  title: Text("RAM 16GB +8GB"),
+                  onChanged: (val) {
+                    setState(() {
+                      myvalue = val!;
+                    });
+                  },
+                ),
+                CheckboxListTile(
+                  value: myval,
+                  title: Text("SSD 500 GB"),
+                  onChanged: (val) {
+                    setState(() {
+                      myval = val!;
+                    });
+                  },
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
-    ));
+        ));
   }
 }
